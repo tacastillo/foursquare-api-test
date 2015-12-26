25f032a709c500ae3c8cb3b9bd76b6753b074bcd
Timothy Castillo <tacastillo@usfca.edu>
2015-12-25 20:59:31 -0800

    incremental progress

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+window.Instagram = {
+	/**
+	 * Stores Application Credentials
+	 */
+	config: {
+		client_id: 	"d056411e9a32439193811e9b33e9e20e"
+	},
+
+	init: function(options) {
+		options = options || {};
+		this.config.access_token = options.access_token;
+	},
+
+
+}
+
 $(document).ready(function() {
 	$('#container').text("This is bull");
 });
